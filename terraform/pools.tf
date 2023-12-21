3cd24bfdd3a40d9bc90b9ba4d187983e7aee6ae7
Julien HOMMET <jho@ik.me>
2023-12-21 21:37:33 +0100

    tf: provider+vars+pool resource

---
+++ terraform/pools.tf
@@ -0,0 +1,20 @@
+
+resource "proxmox_virtual_environment_pool" "infra" {
+  comment = "Managed by Terraform"
+  pool_id = "infra"
+}
+
+resource "proxmox_virtual_environment_pool" "modeles" {
+  comment = "Managed by Terraform"
+  pool_id = "modeles"
+}
+
+resource "proxmox_virtual_environment_pool" "prod" {
+  comment = "Managed by Terraform"
+  pool_id = "production"
+}
+
+resource "proxmox_virtual_environment_pool" "preprod" {
+  comment = "Managed by Terraform"
+  pool_id = "pre-production"
+}
---
+++ terraform/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "0.41.0"
+    }
+  }
+}
+
+provider "proxmox" {
+  endpoint  = var.pve_host_address
+  api_token = var.pve_api_token
+  insecure  = true
+  ssh {
+    agent    = true
+    username = var.pve_api_user
+  }
+  tmp_dir = var.tmp_dir
+}
