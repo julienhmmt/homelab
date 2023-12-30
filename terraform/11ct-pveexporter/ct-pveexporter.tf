2bd582bc034666c832a96700cfd0669051208d90
Julien HOMMET <jho@ik.me>
2023-12-30 16:44:02 +0100

    tf: pveexporter with HA in place of 3 ct (redundant)

--- terraform/11ct-pveexporter/ct-pveexporter.tf
+++ terraform/11ct-pveexporter/ct-pveexporter.tf
@@ -52,7 +52,7 @@ resource "proxmox_virtual_environment_container" "pveexporter_1" {
   }
 
   initialization {
-    hostname = "pveexporter-w3p241"
+    hostname = "pveexporter"
 
     dns {
       domain  = var.dns_domain
@@ -81,144 +81,169 @@ resource "proxmox_virtual_environment_container" "pveexporter_1" {
   }
 }
 
-###
-resource "random_password" "pveexporter_root_password2" {
-  length           = 24
-  override_special = "_%@"
-  special          = true
+resource "proxmox_virtual_environment_hagroup" "ha_infra" {
+  group   = "ha_infra"
+  comment = "Managed by Terraform. Group for HA, specially infra CT & VM"
+  nodes = {
+    node1 = null
+    node2 = 2
+    node3 = 1
+  }
+
+  restricted  = true
+  no_failback = false
 }
 
-output "pveexporter_root_password2" {
-  value     = random_password.pveexporter_root_password2.result
-  sensitive = true
-}
-
-resource "proxmox_virtual_environment_container" "pveexporter_2" {
-  description   = "Used only for PVE Exporter. Managed by Terraform"
-  node_name     = "w3p242"
-  pool_id       = "infra"
-  start_on_boot = true
-  tags          = ["linux", "infra", "monitoring"]
-  unprivileged  = true
-  vm_id         = 242004
-
-  cpu {
-    architecture = "amd64"
-    cores        = 1
-  }
-
-  disk {
-    datastore_id = var.ct_datastore_storage_location
-    size         = var.ct_disk_size
-  }
-
-  memory {
-    dedicated = var.ct_memory
-    swap      = 0
-  }
-
-  operating_system {
-    template_file_id = proxmox_virtual_environment_file.debian_container_template.id
-    type             = var.os_type
-  }
-
-  initialization {
-    hostname = "pveexporter-w3p242"
-
-    dns {
-      domain  = var.dns_domain
-      servers = var.dns_servers
-    }
-
-    ip_config {
-      ipv4 {
-        address = "172.16.2.4/16"
-        gateway = var.gateway
-      }
-    }
-    user_account {
-      keys     = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEEHKEQ6FLrn8b85ClMxvu04DbAiyMZ5tf5ktL4xEpSZ mettmett@JH-LVL10"]
-      password = random_password.pveexporter_root_password2.result
-    }
-  }
-  network_interface {
-    name       = var.ct_bridge
-    rate_limit = var.ct_nic_rate_limit
-  }
-
-  features {
-    nesting = true
-    fuse    = false
-  }
+resource "proxmox_virtual_environment_haresource" "pveexporter_ct" {
+  comment = "Managed by Terraform"
+  depends_on = [
+    proxmox_virtual_environment_hagroup.ha_infra
+  ]
+  group        = "ha_infra"
+  max_relocate = 1
+  max_restart  = 1
+  resource_id  = "ct:241004"
+  state        = "started"
 }
 
 ###
-resource "random_password" "pveexporter_root_password3" {
-  length           = 24
-  override_special = "_%@"
-  special          = true
-}
+# resource "random_password" "pveexporter_root_password2" {
+#   length           = 24
+#   override_special = "_%@"
+#   special          = true
+# }
 
-output "pveexporter_root_password3" {
-  value     = random_password.pveexporter_root_password3.result
-  sensitive = true
-}
+# output "pveexporter_root_password2" {
+#   value     = random_password.pveexporter_root_password2.result
+#   sensitive = true
+# }
 
-resource "proxmox_virtual_environment_container" "pveexporter_3" {
-  description   = "Used only for PVE Exporter. Managed by Terraform"
-  node_name     = "w3p243"
-  pool_id       = "infra"
-  start_on_boot = true
-  tags          = ["linux", "infra", "monitoring"]
-  unprivileged  = true
-  vm_id         = 243004
+# resource "proxmox_virtual_environment_container" "pveexporter_2" {
+#   description   = "Used only for PVE Exporter. Managed by Terraform"
+#   node_name     = "w3p242"
+#   pool_id       = "infra"
+#   start_on_boot = true
+#   tags          = ["linux", "infra", "monitoring"]
+#   unprivileged  = true
+#   vm_id         = 242004
 
-  cpu {
-    architecture = "amd64"
-    cores        = 1
-  }
+#   cpu {
+#     architecture = "amd64"
+#     cores        = 1
+#   }
 
-  disk {
-    datastore_id = var.ct_datastore_storage_location
-    size         = var.ct_disk_size
-  }
+#   disk {
+#     datastore_id = var.ct_datastore_storage_location
+#     size         = var.ct_disk_size
+#   }
 
-  memory {
-    dedicated = var.ct_memory
-    swap      = 0
-  }
+#   memory {
+#     dedicated = var.ct_memory
+#     swap      = 0
+#   }
 
-  operating_system {
-    template_file_id = proxmox_virtual_environment_file.debian_container_template.id
-    type             = var.os_type
-  }
+#   operating_system {
+#     template_file_id = proxmox_virtual_environment_file.debian_container_template.id
+#     type             = var.os_type
+#   }
 
-  initialization {
-    hostname = "pveexporter-w3p243"
+#   initialization {
+#     hostname = "pveexporter-w3p242"
 
-    dns {
-      domain  = var.dns_domain
-      servers = var.dns_servers
-    }
+#     dns {
+#       domain  = var.dns_domain
+#       servers = var.dns_servers
+#     }
 
-    ip_config {
-      ipv4 {
-        address = "172.16.3.4/16"
-        gateway = var.gateway
-      }
-    }
-    user_account {
-      keys     = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEEHKEQ6FLrn8b85ClMxvu04DbAiyMZ5tf5ktL4xEpSZ mettmett@JH-LVL10"]
-      password = random_password.pveexporter_root_password3.result
-    }
-  }
-  network_interface {
-    name       = var.ct_bridge
-    rate_limit = var.ct_nic_rate_limit
-  }
+#     ip_config {
+#       ipv4 {
+#         address = "172.16.2.4/16"
+#         gateway = var.gateway
+#       }
+#     }
+#     user_account {
+#       keys     = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEEHKEQ6FLrn8b85ClMxvu04DbAiyMZ5tf5ktL4xEpSZ mettmett@JH-LVL10"]
+#       password = random_password.pveexporter_root_password2.result
+#     }
+#   }
+#   network_interface {
+#     name       = var.ct_bridge
+#     rate_limit = var.ct_nic_rate_limit
+#   }
 
-  features {
-    nesting = true
-    fuse    = false
-  }
-}
+#   features {
+#     nesting = true
+#     fuse    = false
+#   }
+# }
+
+# ###
+# resource "random_password" "pveexporter_root_password3" {
+#   length           = 24
+#   override_special = "_%@"
+#   special          = true
+# }
+
+# output "pveexporter_root_password3" {
+#   value     = random_password.pveexporter_root_password3.result
+#   sensitive = true
+# }
+
+# resource "proxmox_virtual_environment_container" "pveexporter_3" {
+#   description   = "Used only for PVE Exporter. Managed by Terraform"
+#   node_name     = "w3p243"
+#   pool_id       = "infra"
+#   start_on_boot = true
+#   tags          = ["linux", "infra", "monitoring"]
+#   unprivileged  = true
+#   vm_id         = 243004
+
+#   cpu {
+#     architecture = "amd64"
+#     cores        = 1
+#   }
+
+#   disk {
+#     datastore_id = var.ct_datastore_storage_location
+#     size         = var.ct_disk_size
+#   }
+
+#   memory {
+#     dedicated = var.ct_memory
+#     swap      = 0
+#   }
+
+#   operating_system {
+#     template_file_id = proxmox_virtual_environment_file.debian_container_template.id
+#     type             = var.os_type
+#   }
+
+#   initialization {
+#     hostname = "pveexporter-w3p243"
+
+#     dns {
+#       domain  = var.dns_domain
+#       servers = var.dns_servers
+#     }
+
+#     ip_config {
+#       ipv4 {
+#         address = "172.16.3.4/16"
+#         gateway = var.gateway
+#       }
+#     }
+#     user_account {
+#       keys     = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEEHKEQ6FLrn8b85ClMxvu04DbAiyMZ5tf5ktL4xEpSZ mettmett@JH-LVL10"]
+#       password = random_password.pveexporter_root_password3.result
+#     }
+#   }
+#   network_interface {
+#     name       = var.ct_bridge
+#     rate_limit = var.ct_nic_rate_limit
+#   }
+
+#   features {
+#     nesting = true
+#     fuse    = false
+#   }
+# }
