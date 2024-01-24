d4e6a62d3c29cd38bdbc2547919951772909a8b5
JHO <jho@ik.me>
2024-01-24 20:51:27 +0100

    chore: rename folders and vars from rke2 to k3s

---
+++ terraform/31vm-k3s-ha/ha.tf
@@ -0,0 +1,50 @@
+// HA configuration
+
+resource "proxmox_virtual_environment_hagroup" "ha_vm_k3s" {
+  group   = "ha_vm_k3s"
+  comment = "Managed by Terraform. Group for HA specially for k3s CT & VM"
+  nodes = {
+    node1 = null
+    node2 = 2
+    node3 = 1
+  }
+
+  restricted  = true
+  no_failback = false
+}
+
+resource "proxmox_virtual_environment_haresource" "ha_vm_k3s_master" {
+  depends_on = [
+    proxmox_virtual_environment_hagroup.ha_vm_k3s
+  ]
+  comment      = "Managed by Terraform."
+  group        = proxmox_virtual_environment_hagroup.ha_vm_k3s.group
+  max_relocate = 1
+  max_restart  = 2
+  resource_id  = "vm:241010"
+  state        = "started"
+}
+
+resource "proxmox_virtual_environment_haresource" "ha_vm_k3s_worker1" {
+  depends_on = [
+    proxmox_virtual_environment_hagroup.ha_vm_k3s
+  ]
+  comment      = "Managed by Terraform."
+  group        = proxmox_virtual_environment_hagroup.ha_vm_k3s.group
+  max_relocate = 1
+  max_restart  = 2
+  resource_id  = "vm:242010"
+  state        = "started"
+}
+
+resource "proxmox_virtual_environment_haresource" "ha_vm_k3s_worker2" {
+  depends_on = [
+    proxmox_virtual_environment_hagroup.ha_vm_k3s
+  ]
+  comment      = "Managed by Terraform."
+  group        = proxmox_virtual_environment_hagroup.ha_vm_k3s.group
+  max_relocate = 1
+  max_restart  = 2
+  resource_id  = "vm:243010"
+  state        = "started"
+}
