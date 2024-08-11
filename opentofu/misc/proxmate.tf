fb2cf5fae8b063ba7d184d7ed5fe9f8f360d6a02
JHMMT <jho@ik.me>
2024-08-11 22:23:38 +0200

    feat(opentofu): create pve user+role for proxmate ios app

---
+++ opentofu/misc/proxmate.tf
@@ -0,0 +1,19 @@
+resource "proxmox_virtual_environment_user" "proxmate_user" {
+  acl {
+    path      = "/"
+    propagate = true
+    role_id   = proxmox_virtual_environment_role.proxmate_role.role_id
+  }
+
+  comment  = "Managed by OpenTofu. Used only by the application Proxmate for iOS."
+  password = data.sops_file.pve_secrets.data["proxmate_password"]
+  user_id  = "proxmate@pve"
+}
+
+resource "proxmox_virtual_environment_role" "proxmate_role" {
+  role_id = "proxmate_role"
+
+  privileges = [
+    "Datastore.Allocate", "Datastore.AllocateSpace", "Datastore.AllocateTemplate", "Datastore.Audit", "Group.Allocate", "Permissions.Modify", "Pool.Audit", "SDN.Allocate", "SDN.Audit", "Sys.Audit", "User.Modify", "VM.Audit", "VM.PowerMgmt", "Sys.Console",
+  ]
+}
