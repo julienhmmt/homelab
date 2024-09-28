addae85083c36e43b6ce558636e42735eea222cb
JHMMT <jho@ik.me>
2024-09-28 22:00:44 +0200

    arch ct OK

--- opentofu/ct/lxc.tf
+++ opentofu/ct/lxc.tf
@@ -1,112 +1,115 @@
-# # see https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/password.html. It will download "hashicorp/random" provider
-# resource "random_password" "ct_root_password" {
-#   for_each         = var.ct
-#   length           = 24
-#   override_special = "_%@"
-#   special          = true
-# }
+# see https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/password.html. It will download "hashicorp/random" provider
+resource "random_password" "ct_root_password" {
+  for_each         = var.ct
+  length           = 24
+  override_special = "_%@"
+  special          = true
+}
 
-# output "ct_root_password" {
-#   value     = { for key, pwd in random_password.ct_root_password : key => pwd.result }
-#   sensitive = true
-# }
+output "ct_root_password" {
+  value     = { for key, pwd in random_password.ct_root_password : key => pwd.result }
+  sensitive = true
+}
 
-# # location of containers templates
-# resource "proxmox_virtual_environment_file" "debian_container_template" {
-#   content_type = "vztmpl"
-#   datastore_id = "local"
-#   node_name    = "pve1"
+# location of containers templates
+resource "proxmox_virtual_environment_file" "arch_container_template" {
+  content_type = "vztmpl"
+  datastore_id = "iso"
+  node_name    = "proxmox"
 
-#   source_file {
-#     path = "http://download.proxmox.com/images/system/debian-12-standard_12.2-1_amd64.tar.zst"
-#   }
-# }
+  source_file {
+    path = "http://download.proxmox.com/images/system/archlinux-base_20240911-1_amd64.tar.zst"
+  }
+}
 
-# resource "proxmox_virtual_environment_container" "ct" {
-#   depends_on = [
-#     proxmox_virtual_environment_file.debian_container_template,
-#     proxmox_virtual_environment_file.hook_script,
-#     random_password.ct_root_password
-#   ]
-#   for_each = var.ct
+resource "proxmox_virtual_environment_container" "ct" {
+  depends_on = [
+    proxmox_virtual_environment_file.arch_container_template,
+    # proxmox_virtual_environment_file.hook_script,
+    random_password.ct_root_password
+  ]
+  for_each = var.ct
 
-#   description         = each.value.description
-#   hook_script_file_id = each.value.hook_script_file_id
-#   node_name           = "pve1"
-#   pool_id             = each.value.pool_id
-#   start_on_boot       = each.value.start_on_boot
-#   tags                = each.value.tags
-#   unprivileged        = each.value.unprivileged
-#   vm_id               = each.value.id
+  description         = each.value.description
+  # hook_script_file_id = each.value.hook_script_file_id
+  node_name           = "proxmox"
+  pool_id             = each.value.pool_id
+  start_on_boot       = each.value.start_on_boot
+  tags                = each.value.tags
+  unprivileged        = each.value.unprivileged
+  vm_id               = each.value.id
 
-#   cpu {
-#     architecture = "amd64"
-#     cores        = each.value.cpu_cores
-#   }
+  cpu {
+    architecture = "amd64"
+    cores        = each.value.cpu_cores
+  }
 
-#   disk {
-#     datastore_id = "local-zfs"
-#     size         = each.value.disk_size
-#   }
+  disk {
+    datastore_id = "ct"
+    size         = each.value.disk_size
+  }
 
-#   features {
-#     nesting = true
-#     fuse    = false
-#   }
+  features {
+    nesting = true
+    fuse    = false
+  }
 
-#   initialization {
-#     hostname = each.value.hostname
+  initialization {
+    hostname = each.value.hostname
 
-#     dns {
-#       domain  = each.value.domain
-#       servers = ["192.168.1.2"]
-#     }
+    dns {
+      domain  = each.value.domain
+      servers = ["192.168.1.2"]
+    }
 
-#     ip_config {
-#       ipv4 {
-#         address = each.value.ipv4
-#         gateway = "192.168.1.254"
-#       }
-#     }
-#     user_account {
-#       keys     = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEEHKEQ6FLrn8b85ClMxvu04DbAiyMZ5tf5ktL4xEpSZ mettmett@JH-LVL10"]
-#       password = random_password.ct_root_password[each.key].result
-#     }
-#   }
+    ip_config {
+      ipv4 {
+        address = each.value.ipv4
+        gateway = "192.168.1.254"
+      }
+      ipv6 {
+        address = "auto"
+      }
+    }
+    user_account {
+      keys     = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEEHKEQ6FLrn8b85ClMxvu04DbAiyMZ5tf5ktL4xEpSZ mettmett@JH-LVL10"]
+      password = random_password.ct_root_password[each.key].result
+    }
+  }
 
-#   memory {
-#     dedicated = each.value.ram
-#     swap      = each.value.swap
-#   }
+  memory {
+    dedicated = each.value.ram
+    swap      = each.value.swap
+  }
 
-#   network_interface {
-#     name        = "vmbr0"
-#     mac_address = each.value.net_mac_address
-#     rate_limit  = each.value.net_rate_limit
-#   }
+  network_interface {
+    name        = "vmbr0"
+    mac_address = each.value.net_mac_address
+    rate_limit  = each.value.net_rate_limit
+  }
 
-#   operating_system {
-#     template_file_id = proxmox_virtual_environment_file.debian_container_template.id
-#     type             = "debian"
-#   }
+  operating_system {
+    template_file_id = proxmox_virtual_environment_file.arch_container_template.id
+    type             = "archlinux"
+  }
 
-#   startup {
-#     order    = each.value.startup_order
-#     up_delay = each.value.startup_up_delay
-#   }
+  startup {
+    order    = each.value.startup_order
+    up_delay = each.value.startup_up_delay
+  }
 
-#   timeout_create = 120
-#   timeout_delete = 120
-#   timeout_update = 60
-# }
+  timeout_create = 120
+  timeout_delete = 120
+  timeout_update = 60
+}
 
 # resource "proxmox_virtual_environment_file" "hook_script" {
 #   for_each = var.hook_scripts
 
 #   content_type = "snippets"
-#   datastore_id = "local"
-#   file_mode = "0700"
-#   node_name    = "pve1"
+#   datastore_id = "ct"
+#   file_mode    = "0700"
+#   node_name    = "proxmox"
 
 #   source_raw {
 #     data      = each.value
