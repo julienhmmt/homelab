465a9ccbabcf825fed20f7aaf03ac7e3ebfd509a
jho <jho@ik.me>
2025-01-03 21:29:39 +0100

    ok

--- opentofu/talos-cluster/main.tf
+++ opentofu/talos-cluster/main.tf
@@ -5,17 +5,21 @@
 resource "talos_machine_secrets" "this" {}
 
 data "talos_machine_configuration" "controlplane" {
-  cluster_name     = var.cluster_name
-  cluster_endpoint = var.cluster_endpoint
-  machine_type     = "controlplane"
-  machine_secrets  = talos_machine_secrets.this.machine_secrets
+  cluster_name       = var.cluster_name
+  cluster_endpoint   = var.cluster_endpoint
+  machine_type       = "controlplane"
+  machine_secrets    = talos_machine_secrets.this.machine_secrets
+  talos_version      = "1.9.1"
+  kubernetes_version = "1.32.0"
 }
 
 data "talos_machine_configuration" "worker" {
-  cluster_name     = var.cluster_name
-  cluster_endpoint = var.cluster_endpoint
-  machine_type     = "worker"
-  machine_secrets  = talos_machine_secrets.this.machine_secrets
+  cluster_name       = var.cluster_name
+  cluster_endpoint   = var.cluster_endpoint
+  machine_type       = "worker"
+  machine_secrets    = talos_machine_secrets.this.machine_secrets
+  talos_version      = "1.9.1"
+  kubernetes_version = "1.32.0"
 }
 
 data "talos_client_configuration" "this" {
