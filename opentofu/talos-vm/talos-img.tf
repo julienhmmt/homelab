e4c499956ed32af252a540bf81bfc2d54b694cd7
jho <jho@ik.me>
2025-01-03 22:03:57 +0100

    sd

--- opentofu/talos-vm/talos-img.tf
+++ opentofu/talos-vm/talos-img.tf
@@ -1,11 +1,9 @@
-# resource "proxmox_virtual_environment_download_file" "talos_img" {
+resource "proxmox_virtual_environment_download_file" "talos_img" {
 
-#   node_name    = "miniquarium"
-#   content_type = "iso"
-#   datastore_id = "local-nvme-vm"
-
-#   file_name               = "talos-v1.9.1.img"
-#   url = "https://factory.talos.dev/image/698322b7ce860b1f034de6bc66315576c3b957ceff0934c8120891753f68de82/v1.9.1/metal-amd64.raw.zst"
-#   decompression_algorithm = "gz"
-#   overwrite               = false
-# }
+  node_name    = "miniquarium"
+  content_type = "iso"
+  datastore_id = "local-nvme-vm"
+  file_name    = "talos-v1.9.1.qcow2.img"
+  url          = "https://factory.talos.dev/image/698322b7ce860b1f034de6bc66315576c3b957ceff0934c8120891753f68de82/v1.9.1/metal-amd64.qcow2"
+  overwrite    = false
+}
