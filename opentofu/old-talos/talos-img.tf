0a2e4b7911c3ccd7921c8b9ea80f481192b72b3f
jho <jho@ik.me>
2025-03-22 13:55:10 +0100

    Remove deprecated configuration and state files for old Talos and VM setups

--- opentofu/old-talos/talos-img.tf
+++
@@ -1,8 +0,0 @@
-resource "proxmox_virtual_environment_download_file" "talos_img" {
-  node_name    = "miniquarium"
-  content_type = "iso"
-  datastore_id = "local"
-  file_name    = "talos-v1.9.2.qcow2.img"
-  url          = "https://factory.talos.dev/image/89502df2dee302dd817cc88d7f0268e3cd2c1bb4eaa4e21445f35d0cf9324497/v1.9.2/metal-amd64.qcow2"
-  overwrite    = false
-}
